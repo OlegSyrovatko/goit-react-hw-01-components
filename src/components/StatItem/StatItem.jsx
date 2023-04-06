b4ece59e61f1491103e1a22b86a53dff70ddf96c
Oleg <sirov@ukr.net>
2023-04-06 19:22:27 +0300

    fix stat error

--- src/components/StatItem/StatItem.jsx
+++ src/components/StatItem/StatItem.jsx
@@ -1,14 +1,14 @@
 
 import PropTypes from 'prop-types';
 
-import { H3 } from './StatItem.styled';
+// import { H3 } from './StatItem.styled';
 
 
 export const StatItem = ({id, label, percentage }) => {
 	return (
 			<>
-				<H3>{label} </H3>
-				<H3>{percentage} </H3>
+				{label} 
+				{/* <H3>{percentage} </H3> */}
 			</>
 	);
 };
